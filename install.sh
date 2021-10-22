0abf89b4015cc07bab4e03817ca0efe79ad4840a
vatsalroot <54887589+vatsalroot@users.noreply.github.com>
2021-10-22 16:12:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 if [[ $EUID -ne 0 ]]; then
    echo ""
-   echo -e "\e[1;91m[-] Please run this script as a root [sudo bash $0]"
+   echo -e "\e[1;91m[-] Please run this script as a root [sudo bash install.sh]"
    echo -e "\e[0m"
    exit 1
 fi
